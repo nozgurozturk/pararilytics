a1396b24c11995d1ced4ab5797b6979eac31a5b7
n.ozgurozturk <ozgur@nozgurozturk.com>
2021-11-11 00:27:03 +0100

    update functions

--- collector/collector.go
+++ collector/collector.go
@@ -5,64 +5,66 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/cloudevents/sdk-go/v2/event"
 	"log"
 	"os"
 	"time"
 )
 
-type MessagePublishedData struct {
-	Message PubSubMessage
-}
-
 type PubSubMessage struct {
 	Data []byte `json:"data"`
 }
 
-func StoreHouseFromCity(ctx context.Context, e event.Event) error {
+func StoreHouseFromCity(ctx context.Context, msg PubSubMessage) error {
 	projectID := os.Getenv("PROJECT_ID")
 
-	fireStoreClient, err := firestore.NewClient(ctx, projectID)
-
-	var msg MessagePublishedData
-	if err := e.DataAs(&msg); err != nil {
-		return fmt.Errorf("event.DataAs: %v", err)
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatal(err)
+		return err
 	}
-
 	var houses []House
 
-	if err := json.Unmarshal(msg.Message.Data, &houses); err != nil {
+	if err := json.Unmarshal(msg.Data, &houses); err != nil {
 		return fmt.Errorf("json.Unmarshal: %v", err)
 	}
 
-	var fireStoreHouses map[string]House
-
-	for _, house := range houses {
-		fireStoreHouses[house.ID] = house
+	if len(houses) < 1 {
+		log.Printf("house length is 0")
+		return nil
 	}
 
-	_, err = fireStoreClient.Collection("cities").Doc(houses[0].Address.City).Set(ctx, fireStoreHouses, firestore.MergeAll)
+	batch := client.Batch()
 
-	if err != nil {
-		// Handle any errors in an appropriate way, such as returning them.
-		log.Printf("An error has occurred: %s", err)
+	houseCollection := client.Collection("cities").Doc(houses[0].Address.City).Collection("houses")
+
+	for _, house := range houses {
+		if house.ID != "" {
+			doc := houseCollection.Doc(house.ID)
+			batch.Set(doc, house)
+		}
+	}
+
+	if _, err := batch.Commit(ctx); err != nil {
+		log.Printf("An error occured %v \n", err)
+		return err
 	}
 
 	return nil
 }
 
 type Address struct {
-	City    string `json:"city,omitempty"`
-	ZipCode string `json:"zip_code,omitempty"`
+	City     string `json:"city,omitempty" firestore:"city,omitempty"`
+	District string `json:"district,omitempty" firestore:"district,omitempty"`
+	ZipCode  string `json:"zip_code,omitempty" firestore:"zip_code,omitempty"`
 }
 
 type House struct {
-	ID        string    `json:"id,omitempty"`
-	URL       string    `json:"url,omitempty"`
-	Price     uint      `json:"price,omitempty"`
-	Area      uint      `json:"area,omitempty"`
-	Address   Address   `json:"address"`
-	Interior  string    `json:"interior,omitempty"`
-	OfferedAt time.Time `json:"offered_at"`
-	CrawledAt time.Time `json:"crawled_at"`
+	ID        string    `json:"id,omitempty" firestore:"id,omitempty"`
+	URL       string    `json:"url,omitempty" firestore:"url,omitempty"`
+	Price     int       `json:"price,omitempty" firestore:"price,omitempty"`
+	Area      int       `json:"area,omitempty" firestore:"area,omitempty"`
+	Address   Address   `json:"address" firestore:"address"`
+	Interior  string    `json:"interior,omitempty" firestore:"interior,omitempty"`
+	OfferedAt time.Time `json:"offered_at" firestore:"offered_at,omitempty"`
+	CrawledAt time.Time `json:"crawled_at" firestore:"crawled_at,omitempty"`
 }
--- collector/go.mod
+++ collector/go.mod
@@ -2,29 +2,11 @@ module github.com/nozgurozturk/pararilytics/collector
 
 go 1.16
 
-require (
-	cloud.google.com/go/firestore v1.5.0
-	github.com/cloudevents/sdk-go/v2 v2.6.1
-)
+require cloud.google.com/go/firestore v1.5.0
 
 require (
 	cloud.google.com/go v0.97.0 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/google/go-cmp v0.5.6 // indirect
-	github.com/googleapis/gax-go/v2 v2.1.1 // indirect
-	github.com/json-iterator/go v1.1.10 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
-	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
-	go.opencensus.io v0.23.0 // indirect
-	golang.org/x/net v0.0.0-20210503060351-7fd8e65b6420 // indirect
 	golang.org/x/oauth2 v0.0.0-20211005180243-6b3c2da341f1 // indirect
-	golang.org/x/sys v0.0.0-20210917161153-d61c044b1678 // indirect
-	golang.org/x/text v0.3.6 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/api v0.58.0 // indirect
-	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20211019152133-63b7e35f4404 // indirect
-	google.golang.org/grpc v1.40.0 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
 )
